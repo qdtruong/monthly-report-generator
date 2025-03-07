bbc70e19b01018d15a4e074cc110402fb9800cf2
Quang D. Truong <86080222+qdtruong@users.noreply.github.com>
2025-03-07 12:53:09 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,17 +4,22 @@ Created on Mon Mar  3 19:30:38 2025
 
 @author: quang.truong
 """
+
+#this only needs to be run if running code from Spyder interpreter (instead of running with Run File/F5)
 #import sys
-#sys.path.append(r"C:\Users\quang.truong\OneDrive - HHS Office of the Secretary\Desktop\New folder")
+#sys.path.append(r"C:\Users\quang.truong\OneDrive - HHS Office of the Secretary\Desktop\Report Generator")
 
-import open_directory  # Imports python1.py
-import monthly_report_extract_final  # Imports python2.py
+import open_directory  # Imports module 1
+import monthly_report_extract_final  # Imports module 2
+import report_output # Imports module 3
 
-# Run the function from python1.py to get the output dataframe
+# Run the function from module 1 to get the output dataframe
 folder_path = open_directory.select_folder() # Replace `some_function` with the actual function that generates df
 file_df = open_directory.create_dataframe_from_folder(folder_path) 
 
-# Pass the output dataframe into python2.py's function
+# Pass the output dataframe into module 2's function
 result = monthly_report_extract_final.combined_results(folder_path,file_df)  # Replace `some_other_function` with the function that accepts df_filenames
 
+# Output report with module 3
+report_output.report_output(result, folder_path)
 
